92a9b3d670ffc48c3ccab94f1c7d331b0048681a
David <josedago1163@gmail.com>
2022-10-10 01:21:05 -0500

    primer intento a ver que tal

--- pregunta.py
+++ pregunta.py
@@ -13,9 +13,29 @@ import pandas as pd
 
 
 def ingest_data():
+    import re
+    
+    i = 0
+    dict_linea = {}
+    df = pd.DataFrame()
+    with open('./clusters_report.txt') as f:
+        for line in f:
 
-    #
-    # Inserte su código aquí
-    #
+            line = re.sub(r"\s+", " ", line)
+            if len(line)>1 and i > 3:
+                if line.split()[0].isnumeric() == True:
+                    try: 
+                        dict_linea['Principales palabras clave'] = ' '.join(dict_linea['Principales palabras clave'])
+                        df = df.append(dict_linea, ignore_index=True)
+                    except: pass
+                    dict_linea = {'Cluster': line.split()[0],
+                                'Cantidad de palabras clave': line.split()[1],
+                                'Porcentaje de palabras clave': ''.join(line.split()[2:4]),
+                                'Principales palabras clave': line.split()[4:]}
+                else: 
+                    dict_linea['Principales palabras clave'].append(' '.join(line.split()))
+            i += 1
+    dict_linea['Principales palabras clave'] = ' '.join(dict_linea['Principales palabras clave'])
+    df = df.append(dict_linea, ignore_index=True)
 
     return df
